4fbcc7e17a54f8149246c137aa925437a35a3277
rafaellore <birdneon3@gmail.com>
2025-02-27 16:39:19 -0300

    feat(courses): adds services

---
+++ src/courses/courses.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { CourseEntity } from './courses.entity';
+
+@Injectable()
+export class CoursesService {
+  courses = [] as CourseEntity[];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: number) {
+    return this.courses.find((course) => course.id === id);
+  }
+
+  create(createCourseDTO: any) {
+    this.courses.push(createCourseDTO);
+  }
+
+  update(id: number, updateCourseDTO: any) {
+    const existingCourse = this.findOne(id);
+    if (existingCourse) {
+      const index = this.courses.findIndex((course) => course.id === id);
+      this.courses[index] = {
+        id,
+        ...updateCourseDTO,
+      };
+    }
+  }
+
+  remove(id: number) {
+    const index = this.courses.findIndex((course) => course.id === id);
+    if (index >= 0) {
+      this.courses.splice(index, 1);
+    }
+  }
+}
