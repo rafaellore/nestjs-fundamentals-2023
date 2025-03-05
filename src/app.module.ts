455d58261db607d7d7093b8a70f6104e269864ec
rafaellore <birdneon3@gmail.com>
2025-03-05 20:40:23 -0300

    feat(database): adding postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { CoursesModule } from './courses/courses.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [CoursesModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
