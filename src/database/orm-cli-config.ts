3ad41f5c435cbe7cc1d3f8239d6fef5cc092f5f5
rafaellore <birdneon3@gmail.com>
2025-03-10 18:08:55 -0300

    feat(migrations): adds course id into courses tags table

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,6 +3,7 @@ import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1694705312546 } from 'src/migrations/1741461533195-createCoursesTable';
 import { CreateTagsTable1741464523004 } from 'src/migrations/1741464523004-createTagsTable';
 import { CreateCoursesTagsTable1695042750118 } from 'src/migrations/1741638924858-createCoursesTagsTable.ts';
+import { AddCoursesIdToCoursesTagsTable1741640205560 } from 'src/migrations/1741640205560-AddCoursesIdToCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
@@ -11,5 +12,6 @@ export const dataSource = new DataSource({
     CreateCoursesTable1694705312546,
     CreateTagsTable1741464523004,
     CreateCoursesTagsTable1695042750118,
+    AddCoursesIdToCoursesTagsTable1741640205560,
   ],
 });
