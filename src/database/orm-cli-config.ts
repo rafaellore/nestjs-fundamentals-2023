4b146ff8e9341d6a748649300ee5da271170c93c
rafaellore <birdneon3@gmail.com>
2025-03-10 17:55:35 -0300

    feat(migrations): adds migration for create courses-tags table

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,14 @@ import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1694705312546 } from 'src/migrations/1741461533195-createCoursesTable';
 import { CreateTagsTable1741464523004 } from 'src/migrations/1741464523004-createTagsTable';
+import { CreateCoursesTagsTable1695042750118 } from 'src/migrations/1741638924858-createCoursesTagsTable.ts';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1694705312546, CreateTagsTable1741464523004],
+  migrations: [
+    CreateCoursesTable1694705312546,
+    CreateTagsTable1741464523004,
+    CreateCoursesTagsTable1695042750118,
+  ],
 });
